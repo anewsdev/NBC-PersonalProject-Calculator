cf6351c8a07f167febf4e45aebe1ec66beac930c
anewsdev <anewsdev@gmail.com>
2024-09-10 16:19:44 +0900

    test commit

---
+++ src/com/example/calculator/App.java
@@ -0,0 +1,10 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //github 테스트
+    }
+}
